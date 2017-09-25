00559243ed8b4ac9922e584fd615adeba46f517b
Jefry S.A Putra <32105041+JefrySAP@users.noreply.github.com>
2017-09-25 13:23:31 +0700

    Add files via upload

---
+++ Tugas1-04317040.py
@@ -0,0 +1,9 @@
+nama = "jefry saifullah achmad putra"
+nim = "04317040"
+umur = 20
+print ("namaku:" + nama)
+print ("nimku:" + nim)
+print ("umurku:")
+print (umur)
+print ("umur 5 tahun kebelakang:")
+print (umur -5)
